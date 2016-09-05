49768f22487572bfa9c24a3117285bd560782009
fyl123 <2529854689@qq.com>
2016-09-05 10:16:34 +0800

    文件上传修改

--- tpl/Admin/User/user.js
+++ tpl/Admin/User/user.js
@@ -139,6 +139,27 @@ app.controller('UserUploadCtroller', function($scope, Upload) {
 	    	$scope.upload($scope.file);
 	    }
 	};
+	$scope.upload = function (file) {
+        $scope.fileInfo = file;
+        Upload.upload({
+            //服务端接收
+            url: 'Ashx/UploadFile.ashx',
+            //上传的同时带的参数
+            data: { 'username': $scope.username },
+            file: file
+        }).progress(function (evt) {
+            //进度条
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progess:' + progressPercentage + '%' + evt.config.file.name);
+        }).success(function (data, status, headers, config) {
+            //上传成功
+            console.log('file ' + config.file.name + 'uploaded. Response: ' + data);
+            $scope.uploadImg = data;
+        }).error(function (data, status, headers, config) {
+            //上传失败
+            console.log('error status: ' + status);
+        });
+    };
 });
 
 
